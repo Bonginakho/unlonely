511e9c1df3f6d221eb34d79d98f83f0f7da01109
Bongiwe Nkumane <nakhonkumane@gmail.com>
2025-04-07 19:43:10 +0200

    seeded the 12 journal weeks prompt and seeded 5 users

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_07_084036) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_07_174037) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "journals", force: :cascade do |t|
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.string "prompt"
     t.text "content"
     t.datetime "created_at", null: false
