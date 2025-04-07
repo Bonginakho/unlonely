34b3b3aa59c890de36c999b7471352bde1308297
Bongiwe Nkumane <nakhonkumane@gmail.com>
2025-04-07 12:48:33 +0200

    created the 3 routes and user actions

---
+++ app/controllers/journals_controller.rb
@@ -0,0 +1,27 @@
+class JournalsController < ApplicationController
+  before_action :set_journal, only: %I[show edit update]
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @journal.update(journals_params)
+    if @journal.save!
+      redirect_to journals_path(@journal)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def set_journal
+    @journal = Journal.find(params[:id])
+  end
+
+  def journals_params
+    params.require(:journal).permit(:content)
+  end
+end
