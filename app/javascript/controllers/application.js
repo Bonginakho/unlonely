4324fed6a9e93e865008b6584221255684d12f1b
Bongiwe Nkumane <nakhonkumane@gmail.com>
2025-04-10 22:55:26 +0200

    trying to make the journals appear

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,4 +1,13 @@
 import { Application } from "@hotwired/stimulus"
+import { createApp } from "vue";
+import MyComponent from "./components/mycomponent";
+
+// waiting for the DOM to be loaded before the vue instance is initialized and mounted to
+// id in the HTML of my-vue-app
+document.addEventListener("DOMContentLoaded" , () => {
+  const app = createApp(MyComponent);
+  app.mount("#my-vue-app")
+})
 
 const application = Application.start()
 
