874cd29a889fdb1256cb70388214b16bec15d6a2
Sifiso Maye <sifmay98@gmail.com>
2025-05-07 10:20:44 +0200

    chat messages

--- app/javascript/toggle_journal_form.js
+++ app/javascript/toggle_journal_form.js
@@ -1,13 +1,23 @@
   const buttons = document.querySelectorAll('.toggle-form-button');
 
-  buttons.forEach(button => {
-    button.addEventListener("click", (event) => {
-      const form = button.nextElementSibling;
+  document.addEventListener('DOMContentLoaded', () => {
+    buttons.forEach(button => {
+      button.addEventListener("click", (event) => {
+        const form = button.nextElementSibling;
 
-      if (form.style.display === "none") {
-        form.style.display = "block"
-      } else {
-      form.style.display = "none"
-      }
+        if (form.style.display === "none") {
+          form.style.display = "block"
+        } else {
+          form.style.display = "none"
+        }
+      });
+    });
+
+    // Clear form after successful save
+    document.querySelectorAll('form[data-remote="true"]').forEach(form => {
+      form.addEventListener('ajax:success', () => {
+        form.reset();
+        form.style.display = 'none'; // optionally hide the form
+      });
     });
   });
