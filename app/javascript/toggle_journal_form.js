643eb841d87ef57082a2b67b5689ef2b72b4e2dd
Bongiwe Nkumane <nakhonkumane@gmail.com>
2025-05-08 12:16:25 +0200

    error fixed with IIFE declaration

--- app/javascript/toggle_journal_form.js
+++ app/javascript/toggle_journal_form.js
@@ -1,36 +1,9 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//   buttons.forEach(button => {
-//     button.addEventListener("click", (event) => {
-//       const form = button.nextElementSibling;
-
-//       if (form.style.display === "none") {
-//         form.style.display = "block"
-//       } else {
-//         form.style.display = "none"
-//       }
-//     });
-//   });
-
-//   // Clear form after successful save
-//   document.querySelectorAll('form[data-remote="true"]').forEach(form => {
-//     form.addEventListener('ajax:success', () => {
-//       form.reset();
-//       form.style.display = 'none'; // optionally hide the form
-//     });
-//   });
-// });
-
-const buttons = document.querySelectorAll('.toggle-form-button');
-
+(() => {
+  const buttons = document.querySelectorAll('.toggle-form-button');
   buttons.forEach(button => {
     button.addEventListener("click", (event) => {
-      console.log(event);
       const form = button.nextElementSibling;
-
-      if (form.style.display === "none") {
-        form.style.display = "block"
-      } else {
-      form.style.display = "none"
-      }
+      form.style.display = (form.style.display === "none") ? "block" : "none";
     });
   });
+})();
