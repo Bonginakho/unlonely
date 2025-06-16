948d01b4de83fb54f78bde88e93f20a26f49aa88
Bongiwe Nkumane <nakhonkumane@gmail.com>
2025-06-16 11:56:31 +0200

    created a custom route for the guide page

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   resources :reflections, only: %I[edit update]
   get "originstory", to: "pages#originstory"
+  get "guide", to: "pages#guide"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
